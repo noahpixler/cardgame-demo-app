f18cd95c457ec2f7e5a9d5ae02cd442b08cd5668
Noah Pixler <npixler@madisoncollege.edu>
2026-06-01 16:42:57 -0500

    Added Tailwind, DaisyUI, and a Title

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,11 @@ function App() {
 
   return (
     <>
-      <header>
-        <h1>{APP_TITLE}</h1>
+      <header className="py-2 text-center">
+        <h1 className="mb-2 text-4xl font-bold ">
+          {APP_TITLE}
+        </h1>
+        <hr></hr>
       </header>
     </>
   )
