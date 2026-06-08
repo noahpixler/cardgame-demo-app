fb1ac945b7ad45dfe9d045febce15db0b6ae7e3f
Noah Pixler <npixler@madisoncollege.edu>
2026-06-08 16:42:57 -0500

    Added games.ts file to temporarily save games and their data

---
+++ src/data/games.ts
@@ -0,0 +1,18 @@
+export const games = [
+    {
+        id: "poker",
+        name: "Poker",
+    },
+    {
+        id: "uno",
+        name: "Uno"
+    },
+    {
+        id: "skip-bo",
+        name: "Skip-Bo"
+    },
+    {
+        id: "spades",
+        name: "Spades"
+    },
+];
